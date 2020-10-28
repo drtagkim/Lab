d467a29c01af31711594b40b74e51e5274ff1a87
DRTAG <masan.korea@gmail.com>
2020-10-28 16:25:00 +0900

    update

--- R/NavHotel/test.R
+++ R/NavHotel/test.R
@@ -1,7 +1,18 @@
-hotel_key1='Nine_Tree_Premier_Hotel_Insadong'
-target_date='2020-11-01'
 library(lubridate)
 library(dplyr)
 library(RNavHotel)
+library(purrr)
+hotel_key1='Nine_Tree_Premier_Hotel_Insadong'
+target_date='2020-11-01'
+target_date_set=date(target_date)+1:30
+target_date_set=as.character(target_date_set)
+data.frame(x=target_date_set) %>%
+  map(function(x) {
+    cat(x,'\n')
+    query_hotel(hotel_key1,x) %>% update_data_sqlite('test.db')
+  })
+
+
+
 test=query_hotel(hotel_key1,target_date)
 update_data_sqlite(test,'test.db')
