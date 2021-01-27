a1a7e94acbff10e703ef4db59025e5d104ea884f
DRTAG <masan.korea@gmail.com>
2021-01-28 07:30:55 +0900

    util

---
+++ R/util.R
@@ -0,0 +1,9 @@
+#Taekyung Kim
+#
+reqPkg <- function(PKG) {
+  REPO="https://cran.rstudio.com"
+  suppressMessages({
+    if(!require(PKG,character.only=TRUE,quietly=TRUE)) install.packages(PKG,repos=REPO,quiet=TRUE)
+    require(PKG,character.only=TRUE,quietly=TRUE)
+  })
+}
